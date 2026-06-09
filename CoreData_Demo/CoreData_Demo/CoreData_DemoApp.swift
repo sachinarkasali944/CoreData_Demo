7c27a0c2f8da0d97b88d1275b065b3e840381f75
Sachin Arkasali <sachinarkasali762@gmail.com>
2026-06-09 12:02:01 +0530

    Initial Commit for setup

---
+++ CoreData_Demo/CoreData_Demo/CoreData_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_DemoApp.swift
+//  CoreData_Demo
+//
+//  Created by Sachin Arkasali on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
